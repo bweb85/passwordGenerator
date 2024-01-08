c8de6cee7b4c8a2e3a299954a98c59eddd15e09a
bweb85 <146823882+bweb85@users.noreply.github.com>
2024-01-08 21:14:53 +0000

    concat for arrays, logic for randomising password and password generator

--- script.js
+++ script.js
@@ -90,33 +90,74 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-var characters = prompt("How many characters would you like your password to be? Choose between 8 and 16"); 
-var specCharac = prompt("How many special characters would you like to use?");
-var numcharac = prompt("How many numerical characters would you like to use?");
-var lowCasCharac = prompt ("How many lower case characters would you like to use?");
-var upcasCharac = prompt ("How many upper case characters would you like to use?");
+var characters = prompt("How many characters would you like your password to be? Choose between 8 and 128");
+// conditions for password
+if (characters < 8){
+  alert ("Please choose a numercial value between 8 and 128. Start again");
+  return 
+};
+if (characters > 128) {
+  alert ("Please choose a numercial value between 8 and 128. Start again");
+  return 
+};
+if (isNaN(characters) === true) {
+  alert ("Please choose a numercial value between 8 and 128. Start again");
+  return
+};
+// conditions for password characters
+var specCharac = confirm("Would you like special characters?  e.g !,£,% Press ok to confirm or cancel for no.");
+var numCharac = confirm("Would you like numbers? Press ok to confirm or cancel for no.");
+var lowCasCharac = confirm ("Would you like lowercase characters? Press ok to confirm or cancel for no.");
+var upCasCharac = confirm("Would you like upper case characters? Press ok to confirm or cancel for no.");
+if (!specCharac && !numCharac && !lowCasCharac && !upCasCharac) {
+  alert ("You must choose at least one type of character. Please start again");
+  return
 };
 
+// options object for generatePassword function
+var options = {
+  characters: characters, 
+  specCharac: specCharac, 
+  numCharac: numCharac,
+  lowCasCharac: lowCasCharac,
+  upCasCharac: upCasCharac
+};
+return options; 
+};
 
-
-// TODO: Function for getting a random element from an array
+// Function for getting a random element from an array
 function getRandom(arr) {
+var randomIndex = Math.floor(Math.random() * arr.length);
+return arr[randomIndex];
+};
 
-}
-
-// TODO: Function to generate password with user input
+// Function to generate password with user input
 function generatePassword() {
+  var options = getPasswordOptions(); 
 
+// concat user options with arrays
+var allCharacters = [];
+if (options.specCharac) allCharacters = allCharacters.concat(specialCharacters);
+if (options.numCharac) allCharacters = allCharacters.concat(numericCharacters);
+if (options.lowCasCharac) allCharacters = allCharacters.concat(lowerCasedCharacters);
+if (options.upCasCharac) allCharacters = allCharacters.concat(upperCasedCharacters);
+
+// logic to create password using user input options
+var password = "";
+for (var i = 0; i < options.characters; i++) {
+  password += getRandom(allCharacters)
+};
+
+return password;
 }
 
-// TODO: Get references to the #generate element
+// Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
 
-// TODO: Write password to the #password input
+// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
-
   passwordText.value = password;
 }
 
